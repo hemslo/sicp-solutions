355bc10db4193c6dad9cc4378b35d24ee9f96626
Hemslo Wang <hemslo.wang@gmail.com>
2016-02-18 01:18:25 +0800

    Add 1.3

---
+++ ch1/03.scm
@@ -0,0 +1,8 @@
+; Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
+(define (sum-squares-of-two a b)
+  (+ (* a a) (* b b)))
+
+(define (sum-squares-of-two-larger a b c)
+  (cond ((and (<= a b) (<= a c)) (sum-squares-of-two b c))
+        ((and (<= b a) (<= b c)) (sum-squares-of-two a c))
+        ((and (<= c a) (<= c b)) (sum-squares-of-two a b))))
