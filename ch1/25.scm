5819d9fd09361eb7b5289a4d0d23a226f42b4a93
Hemslo Wang <hemslo.wang@gmail.com>
2016-03-03 23:39:58 +0800

    Add 1.25

---
+++ ch1/25.scm
@@ -0,0 +1,11 @@
+; Alyssa P. Hacker complains that we went to a lot of extra work in writing
+; expmod. After all, she says, since we already know how to compute
+; exponentials, we could have simply written
+
+(define (expmod base exp m)
+  (remainder (fast-expt base exp) m))
+
+; Is she correct? Would this procedure serve as well for our fast prime tester?
+; Explain.
+
+; No. The exponentials are too large. More Time and space required.
