a0c8e0e311e3083059a54399bffdc5ec2def44b6
Hemslo Wang <hemslo.wang@gmail.com>
2016-03-12 23:07:59 +0800

    Add 1.34

---
+++ ch1/34.scm
@@ -0,0 +1,15 @@
+; Suppose we define the procedure
+(define (f g) (g 2))
+; Then we have
+(f square)
+4
+(f (lambda (z) (* z (+ z 1))))
+6
+
+; What happens if we (perversely) ask the interpreter to evaluate the
+; combination (f f)? Explain.
+
+; (f f)
+; (f 2)
+; (2 2)
+; The object 2 is not applicable.
