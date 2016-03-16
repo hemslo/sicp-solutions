14ab3c7ed0852989f05018cc95bd9109787e1559
Hemslo Wang <hemslo.wang@gmail.com>
2016-03-17 01:04:49 +0800

    Add gcd to 2.1

--- ch2/01.scm
+++ ch2/01.scm
@@ -3,7 +3,11 @@
 ; is positive, both the numerator and denominator are positive, and if the
 ; rational number is negative, only the numerator is negative.
 
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
 (define (make-rat n d)
-  (if (< d 0)
-      (cons (- n) (- d))
-      (cons n d)))
+  (let ((g ((if (< d 0) - +) (gcd (abs n) (abs d)))))
+    (cons (/ n g) (/ d g))))
