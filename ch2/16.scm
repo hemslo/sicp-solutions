2b0d4ae6317c685439f70cd7bad227c0f1f36511
Hemslo Wang <hemslo.wang@gmail.com>
2016-03-25 00:14:53 +0800

    Add 2.16

---
+++ ch2/16.scm
@@ -0,0 +1,6 @@
+; Explain, in general, why equivalent algebraic expressions may lead to
+; different answers. Can you devise an interval-arithmetic package that does not
+; have this shortcoming, or is this task impossible? (Warning: This problem is
+; very difficult.)
+
+; The laws of algebra should be redefined in interval arithmetic.
