4437be5c312e7f724d53c1c149e07c40132b233a
Hemslo Wang <hemslo.wang@gmail.com>
2016-04-01 23:42:36 +0800

    dd 2.33

---
+++ ch2/33.scm
@@ -0,0 +1,13 @@
+; Fill in the missing expressions to complete the following definitions of some
+; basic list-manipulation operations as accumulations:
+
+(define accumulate fold)
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) '() sequence))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ y 1)) 0 sequence))
