83fc95a0ffc500f3101b22f640f8f6c91268c4e8
Hemslo Wang <hemslo.wang@gmail.com>
2016-04-02 23:24:34 +0800

    Add 2.40

---
+++ ch2/40.scm
@@ -0,0 +1,31 @@
+; Define a procedure unique-pairs that, given an integer n, generates the
+; sequence of pairs (i, j) with 1 ≤ j < i ≤ n. Use unique-pairs to simplify the
+; definition of prime-sum-pairs given above.
+
+(load "../ch1/22.scm")
+
+(define accumulate fold-right)
+
+(define (flatmap proc seq)
+  (accumulate append '() (map proc seq)))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      '()
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (unique-pairs n)
+  (flatmap (lambda (i)
+             (map (lambda (j) (list i j))
+                  (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum? (unique-pairs n))))
