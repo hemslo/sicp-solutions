e37028b170b091b2d58809421e815321a4a52b5f
Hemslo Wang <hemslo.wang@gmail.com>
2016-04-13 01:44:06 +0800

    Add 2.76

---
+++ 76.scm
@@ -0,0 +1,10 @@
+; As a large system with generic operations evolves, new types of data objects
+; or new operations may be needed. For each of the three strategies—generic
+; operations with explicit dispatch, data-directed style, and
+; message-passing-style—describe the changes that must be made to a system in
+; order to add new types or new operations. Which organization would be most
+; appropriate for a system in which new types must often be added? Which would
+; be most appropriate for a system in which new operations must often be added?
+
+; new types -> message-passing-style
+; new operations -> data-directed
