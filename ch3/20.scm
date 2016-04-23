09eddf40fe341e2d70db0d50f4a91974ba769079
Hemslo Wang <hemslo.wang@gmail.com>
2016-04-24 01:03:47 +0800

    Add 3.20

---
+++ ch3/20.scm
@@ -0,0 +1,12 @@
+; Draw environment diagrams to illustrate the evaluation of the sequence of expressions
+
+(define x (cons 1 2))
+(define z (cons x x))
+(set-car! (cdr z) 17)
+(car x)
+; 17
+
+; using the procedural implementation of pairs given above. (Compare Exercise
+; 3.11.)
+
+; skip
