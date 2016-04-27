69559b9f895019cb933ff860e6d7372726e2071c
Hemslo Wang <hemslo.wang@gmail.com>
2016-04-28 02:18:53 +0800

    Add 3.36

---
+++ ch3/36.scm
@@ -0,0 +1,16 @@
+;  Suppose we evaluate the following sequence of expressions in the global
+; environment:
+
+(define a (make-connector))
+(define b (make-connector))
+(set-value! a 10 'user)
+
+; At some time during evaluation of the set-value!, the following expression
+; from the connector’s local procedure is evaluated:
+
+(for-each-except setter inform-about-value constraints)
+
+; Draw an environment diagram showing the environment in which the above
+; expression is evaluated.
+
+; skip
