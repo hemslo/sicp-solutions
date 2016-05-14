0c280d0662be8a01474dd873cec3f5513546b93f
Hemslo Wang <hemslo.wang@gmail.com>
2016-05-14 09:58:35 +0800

    Add 4.1

---
+++ ch4/01.scm
@@ -0,0 +1,28 @@
+; Notice that we cannot tell whether the metacircular evaluator evaluates
+; operands from left to right or from right to left. Its evaluation order is
+; inherited from the underlying Lisp: If the arguments to cons in list-of-values
+; are evaluated from left to right, then list-of-values will evaluate operands
+; from left to right; and if the arguments to cons are evaluated from right to
+; left, then list-of-values will evaluate operands from right to left.
+; Write a version of list-of-values that evaluates operands from left to right
+; regardless of the order of evaluation in the underlying Lisp. Also write a
+; version of list-of-values that evaluates operands from right to left.
+
+
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (eval (first-operand exps) env)
+            (list-of-values (rest-operands exps) env))))
+
+(define (list-of-values-l2r exps env)
+  (if (no-operands? exps)
+      '()
+      (let (first (eval (first-operand exps) env))
+        (cons first (list-of-values-l2r (rest-operands exps env))))))
+
+(define (list-of-values-r2l exps env)
+  (if (no-operands? exps)
+      '()
+      (let (rest (list-of-values-r2l (rest-operands-operand exps) env))
+        (cons (eval (first-operand exps) env) rest))))
