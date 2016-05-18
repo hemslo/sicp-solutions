2caa1254f787e9bafa40a70d9c8416a8165a2c49
Hemslo Wang <hemslo.wang@gmail.com>
2016-05-18 16:47:22 +0800

    Add 4.10

---
+++ ch4/10.scm
@@ -0,0 +1,7 @@
+; By using data abstraction, we were able to write an eval procedure that is
+; independent of the particular syntax of the language to be evaluated. To
+; illustrate this, design and implement a new syntax for Scheme by modifying the
+; procedures in this section, without changing eval or apply.
+
+; if want ot modify set!
+; only need to modify assignment? and eval-assignment.
