98cd7dae945390380c062aea696a944df8584011
Hemslo Wang <hemslo.wang@gmail.com>
2016-05-29 22:18:32 +0800

    Add 4.32

---
+++ ch4/32.scm
@@ -0,0 +1,6 @@
+; Give some examples that illustrate the difference between the streams of
+; Chapter 3 and the “lazier” lazy lists described in this section. How can you
+; take advantage of this extra laziness?
+
+; In chapter 3, car is not delayed.
+; This lazier lazy lists made lazy evaluate without delay and force.
