a5819809a758cf25397750605720d5ee9eeb7044
Hemslo Wang <hemslo.wang@gmail.com>
2016-06-04 04:25:53 +0800

    Add 4.44

---
+++ ch4/44.scm
@@ -0,0 +1,42 @@
+; Exercise 2.42 described the “eight-queens puzzle” of placing queens on a
+; chessboard so that no two attack each other. Write a nondeterministic program
+; to solve this puzzle.
+
+; eight-queens puzzle
+
+(define accumulate fold-right)
+
+(define (flatmap proc seq)
+  (accumulate append '() (map proc seq)))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      '()
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (map
+          (lambda (positions) (require (safe? k positions)) positions)
+          (flatmap
+            (lambda (rest-of-queens)
+              (adjoin-position new-row
+                               (amb (enumerate-interval 1 board-size))
+                               rest-of-queens))
+            (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+(define empty-board '())
+
+(define (safe? k positions)
+  (let ((k-queen (find (lambda (pos) (= k (cdr pos))) positions)))
+    (for-all? (filter (lambda (pos) (not (= k (cdr pos)))) positions)
+              (lambda (queen)
+                (and (not (= (car queen) (car k-queen)))
+                     (not (= (abs (- (car queen) (car k-queen)))
+                             (abs (- (cdr queen) (cdr k-queen))))))))))
+
+(define (adjoin-position new-row k rest-of-queens)
+  (cons (cons new-row k) rest-of-queens))
