42ce32d6ae6e9104db9faa500e83329c66bb81b0
Hemslo Wang <hemslo.wang@gmail.com>
2022-11-17 23:16:34 +1100

    Add 1.21 js

--- ch1/21.rkt
+++ ch1/21.rkt
@@ -1,7 +1,11 @@
+#lang sicp
+
 ; Use the smallest-divisor procedure to find the smallest divisor of each of the
 ; following numbers: 199, 1999, 19999.
 
 (define (smallest-divisor n) (find-divisor n 2))
+
+(define (square x) (* x x))
 
 (define (find-divisor n test-divisor)
   (cond ((> (square test-divisor) n) n)
@@ -11,10 +15,10 @@
 (define (divides? a b) (= (remainder b a) 0))
 
 (smallest-divisor 199)
-199
+;199
 
 (smallest-divisor 1999)
-1999
+;1999
 
-(smallest-divisor 1999)
-7
+(smallest-divisor 19999)
+;7
